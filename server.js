56cf54b715f6a471ec199f089f109cd1d610bf95
dibyn <dibyn@uxcam.com>
2021-10-30 11:16:26 +0545

    added route config models

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
-const app = express()
-app.get('/', (req,res) => res.send('API Running'))
+const connectDB = require('./config/db');
+const app = express();
 const PORT = process.env.PORT || 5000
+//Connect db
+connectDB();
+//Init Middleware
+app.use(express.json({ extended: false }))
+app.get('/', (req,res) => res.send('API Running'))
+//define routes
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
