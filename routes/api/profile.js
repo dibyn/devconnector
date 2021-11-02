3c1424e263d6e68aa6027ca9a9509513493e0375
dibyn <dibyn@uxcam.com>
2021-11-02 12:08:04 +0545

    add github profile get request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require('../../middleware/auth')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 const router = express.Router()
+const config = require('../../config/default.json')
+const request = require('request')
 //@route GET api/profile/me
 //@desc Get current users profile
 //@access Private
@@ -269,6 +271,28 @@ router.delete('/education/:education_id', auth, async (req, res) => {
     }
   } catch (error) {
     console.error(error)
+    res.status(500).send('Server Error')
+  }
+})
+//@route DELETE api/profile/github/:username
+//@desc get user repos from github
+//@access public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config['githubClientId']}&client_secret=${config['githubSecret']}}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node-js' },
+    }
+    request(options, (error, response, body) => {
+      if (error) console.error(error)
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No profile found' })
+      }
+      res.json(JSON.parse(body))
+    })
+  } catch (error) {
+    console.error(error.message)
     res.status(500).send('Server Error')
   }
 })
