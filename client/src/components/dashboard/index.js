5f138300e0ab48f824440d8a6b1ceef97ac0e5d2
dibyn <dibyn@uxcam.com>
2021-11-04 17:38:44 +0545

    profile reducer and get current profile

--- client/src/components/dashboard/index.js
+++ client/src/components/dashboard/index.js
@@ -1,4 +1,13 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCurrentProfile } from '../../actions/profile'
 const Dashboard = () => {
+  const dispatch = useDispatch()
+  const { authentication, profile } = useSelector((state) => state)
+  useEffect(() => {
+    dispatch(getCurrentProfile())
+    return () => {}
+  }, [])
   return <div>Dashboard</div>
 }
 export default Dashboard
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import alertReducer from './alert'
 import authReducer from './auth'
+import profileReducer from './profile'
 export default combineReducers({
   alerts: alertReducer,
   authentication: authReducer,
+  profile: profileReducer
 })
